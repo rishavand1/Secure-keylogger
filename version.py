5eab0fc7e50c09ac97592c84f3e6315a95cd47cf
rishavand <anandrishav2228@gmail.com>
2024-04-25 20:36:59 +0530

    keylogger

---
+++ version.py
@@ -0,0 +1,12 @@
+
+
+name = "Secyour"
+
+description = "Simple Python Keylogger"
+
+version = "1.1.1"
+
+license = "GPL"
+author = "Rishav anand,Rishabh Pandey,Sameer Gautam,Sanjay Tiwary"
+author_email = "anandrishav2228@gmail.com"
+platform = "Windows NT/2000/XP/Vista, Linux"
